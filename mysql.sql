6b0fc6652dcbf138c5c7d0e739982781b40b86fb
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-12-20 17:27:58 +0100

    add: query 4 and 5

--- mysql.sql
+++ mysql.sql
@@ -17,3 +17,13 @@ FROM `students`
 WHERE DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), `date_of_birth`)), '%Y') + 0 > 30;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+
+SELECT *
+FROM `courses`
+WHERE `period` = 'i semestre' AND `year` = 1; 
+
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+
+SELECT *
+FROM `exams`
+WHERE `date` = '2020-06-20' AND `hour` > '14:00:00';
