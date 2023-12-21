c09b1ef4424c5b32032fb09244078f24bb94f85a
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-12-21 15:42:22 +0100

    add: query for GROUP BY

--- mysql.sql
+++ mysql.sql
@@ -44,3 +44,32 @@ FROM `departments`;
 SELECT COUNT(*) 
 FROM `teachers`
 WHERE `phone` IS NULL;
+
+
+---------------------------------------------------------------------------------------------------------------- GROUP BY
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+
+SELECT COUNT(*) AS `num_students`, YEAR(`enrolment_date`)
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT `office_address`, COUNT(*) AS `num_teachers` 
+FROM `teachers` 
+GROUP BY `office_address`
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+
+SELECT `exam_id`, AVG(`vote`) as `voto_average`
+FROM `exam_student`
+GROUP BY `exam_id`;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT `departments`.`name`, COUNT(`degrees`.`id`) AS `num_courses`
+FROM `departments`
+INNER JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+GROUP BY `departments`.`name`
